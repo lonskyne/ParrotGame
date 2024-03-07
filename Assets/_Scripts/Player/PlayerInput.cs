97a6cb259c8478fcd0c7a1c68a6b8313168dbf2d
Viktor <lonskyne@gmail.com>
2024-03-07 15:06:26 +0100

    Setup postprocessing. Ability input reading.

--- Assets/_Scripts/Player/PlayerInput.cs
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -4,15 +4,87 @@ public class PlayerInput : MonoBehaviour {
 
     private Player playerScript;
 
+    private int curAbility = -1;
+    private int curConnector = -1;
+    private int curAbility2 = -1;
+
+    private float aTimer = 0f;
+    private const float timerWait = 1f;
+    private bool timerActive = false;
+
     void Awake(){
         playerScript = GetComponent<Player>();
     }
 
     
-    void Update(){
+    void Update() {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)){
             playerScript.FireAbility();
         }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+            processAbilityInput(1);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            processAbilityInput(2);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            processAbilityInput(3);
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            processAbilityInput(4);
+
+        if (timerActive)
+        {
+            aTimer -= Time.deltaTime;
+
+            if (aTimer <= 0.0f)
+            {
+                timerEnded();
+                timerActive = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+                processConnectorInput(1);
+
+            if (Input.GetKeyDown(KeyCode.E))
+                processConnectorInput(2);
+        }
+    }
+
+    private void processAbilityInput(int inputValue)
+    {
+        if (curAbility == -1 && curConnector == -1 && curAbility2 == -1)
+        {
+            curAbility = inputValue;
+            aTimer = timerWait;
+            timerActive = true;
+        }
+
+        if (curAbility > -1 && curConnector > -1 && curAbility2 == -1)
+        {
+            curAbility2 = inputValue;
+            aTimer = 0.0f;
+        }
+    }
+
+    private void processConnectorInput(int inputValue)
+    {
+        if(curAbility > -1 && curAbility2 == -1 && curConnector == -1)
+        {
+            curConnector = inputValue;
+            aTimer = timerWait;
+            timerActive = true;
+        }
+    }
+
+    private void timerEnded()
+    {
+        Debug.Log(curAbility + " " + curConnector + " " + curAbility2);
+
+        curAbility = -1;
+        curAbility2 = -1;
+        curConnector = -1;
     }
 
     public Vector2 GetMovementVectorNormalized(){
